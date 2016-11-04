8bc10918bc16cce48b3937abe684fac5309c53b6
David Junquero González <djunquero@edu.tecnocampus.cat>
2016-11-04 11:59:45 +0100

    Diccionari

--- src/paquet2/Application.java
+++ src/paquet2/Application.java
@@ -1,10 +1,17 @@
 package paquet2;
 
+import paquet1.*;
+import java.io.*;
+
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));	
+		String input;
+		input=br.readLine();
+		System.out.println(input);
+		
 	}
 
 }
